3d5321b945fa9bb9f7422f2f653b055706b9cf79
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2026-04-19 15:16:43 +0200

    Initial Commit

---
+++ CompressPdf/CompressPdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompressPdfApp.swift
+//  CompressPdf
+//
+//  Created by Авазбек Надырбек уулу on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CompressPdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
